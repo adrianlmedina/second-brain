394d589377a06cdd192a6d1273c3e83549c9a2b5
adrianjmedina <yellowknight01@gmail.com>
2026-08-11 14:07:02 -0700

    functioning query

--- query.py
+++ query.py
@@ -10,13 +10,11 @@ from llama_index.core.retrievers import VectorIndexRetriever
 from llama_index.vector_stores.chroma import ChromaVectorStore
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.core.postprocessor import SimilarityPostprocessor
 from llama_index.llms.anthropic import Anthropic
 
 load_dotenv()
 
 Settings.embed_model = HuggingFaceEmbedding(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
 
 def load_index():
     #  we connect to the chromadb database and GET a collection called "second_brain"
@@ -47,7 +45,7 @@ def retrieve(query):
     # create a query engine with the retriever
     query_engine = RetrieverQueryEngine(
         retriever=retriever,
-        response_synthesizer=get_response_synthesizer(llm=Anthropic(model="claude-haiku-3-5-20251001", temperature=0.0))
+        response_synthesizer=get_response_synthesizer(llm=Anthropic(model="claude-haiku-4-5-20251001", temperature=0.0))
     )
 
     # perform the query
@@ -57,7 +55,7 @@ def retrieve(query):
 
 
 if __name__ == "__main__":
-    question = "This file will index the contents of a file, store it in ChromaDB collection, and allow the user to query the collection."
+    question = "Where does Leo live?"
     print(f"\nQuestion: {question}\n")
     response = retrieve(question)
     print(f"Answer: {response}")
